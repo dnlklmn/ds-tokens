d257ee44c04e8d103733024a221d156c1fa8fb63
Daniel K <60112552+dnlklmn@users.noreply.github.com>
2021-12-22 14:43:10 +0100

    a
    a

---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Wed, 22 Dec 2021 13:39:09 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
