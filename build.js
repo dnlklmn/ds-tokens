68068945fab0558adb75b0ec8a0131267af1eb55
dnlklmn <pelicansinberlin@gmail.com>
2022-07-07 18:53:58 +0200

    fix

--- build.js
+++ build.js
@@ -12,26 +12,6 @@ StyleDictionaryPackage.registerFormat({
   },
 });
 
-StyleDictionaryPackage.registerTransform({
-  name: "sizes/px",
-  type: "value",
-  matcher: function (prop) {
-    // You can be more specific here if you only want 'em' units for font sizes
-    return [
-      "fontSizes",
-      "fontFamily",
-      "spacing",
-      "borderRadius",
-      "borderWidth",
-      "sizing",
-    ].includes(prop.attributes.category);
-  },
-  transformer: function (prop) {
-    // You can also modify the value here if you want to convert pixels to ems
-    return parseFloat(prop.original.value) + "px";
-  },
-});
-
 function getStyleDictionaryConfig(theme) {
   return {
     source: [`tokens/${theme}.json`],
