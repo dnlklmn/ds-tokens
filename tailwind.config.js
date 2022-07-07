5b08429fb7532215dc0f9ef49cb203a076da4900
dnlklmn <pelicansinberlin@gmail.com>
2022-07-07 16:41:26 +0200

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@ const { filterTokensByType } = require("./fns");
 const tokens = require("./output/global.json");
 
 const colors = filterTokensByType("color", tokens);
+const fontFamily = filterTokensByType("fontFamily", tokens);
 
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors,
+    fontFamily,
   },
   variants: {},
   plugins: [],
